79d7c3094fc3ab0261e601ebd4bc0a7b97110703
Janzen Choi <janzenchoi@yahoo.com>
2023-03-02 22:47:50 +1100

    added vshai_s

--- optimiser/vshai_main.py
+++ optimiser/vshai_main.py
@@ -1,7 +1,8 @@
 from modules.api import API
 api = API("vshai_s", 0)
 api.read_files(["cp_ebsd/t_i2.csv"])
-api.define_model("vshai_s", ["cp_ebsd/ebsd_statistics.csv", 1.0, [1,1,0], [1,1,1], 20, 1e-4/3, 15])
+api.define_model("vshai_s", ["cp_ebsd/ebsd_statistics.csv", 1.0, [1,1,0], [1,1,1], 16, 1e-4/3, 15])
 api.define_errors(["dy_area", "y_area", "y_end"])
+# api.plot_results([30, 60, 20, 0.001, 12])
 api.define_recorder(1, 100)
-api.optimise(10000, 1, 1, 0.65, 0.35)
+api.optimise(10000, 50, 25, 0.65, 0.35)
