c8035364b4f78071916294f8b6dcdf44dd4606f9
janzen <janzenchoi@yahoo.com>
2023-06-10 11:08:24 -0500

    increased record interval

--- optimiser/main.py
+++ optimiser/main.py
@@ -23,5 +23,5 @@ api.read_file("tensile/AirBase_800_D7.csv", True)
 api.add_error("y_area", "creep")
 api.add_error("y_area", "tensile")
 
-api.record(10, 10)
+api.record(50, 10)
 api.optimise(10000, 200, 100, 0.65, 0.35)
