5648441b970a10659ea536f665ece3f1c12ca6f0
janzen <janzenchoi@yahoo.com>
2023-06-28 12:42:44 -0500

    new 750C and 1000C inputs

--- scripts/evpwd_750.py
+++ scripts/evpwd_750.py
@@ -1,20 +1,19 @@
 import sys; sys.path += [".."]
 from moga_neml.api import API
 
-api = API("evp 750")
-api.define_model("evp")
+api = API("evpwd 750")
+api.define_model("evpwd")
 
 api.read_file("creep/inl_2/AirBase_750_137_a.csv", True)
-api.remove_tertiary_creep(200, 0.8)
 api.read_file("creep/inl_2/AirBase_750_118_b.csv", True)
-api.remove_tertiary_creep(200, 0.8)
 api.read_file("creep/inl_2/AirBase_750_95_a.csv", False)
-api.remove_tertiary_creep(50, 0.6)
 api.read_file("tensile/AirBase_750_D6.csv", True)
 
 api.visualise(type="creep")
 api.visualise(type="tensile")
 api.add_error("y_area", "creep")
+api.add_error("x_end", "creep")
+api.add_error("y_end", "creep")
 api.add_error("y_area", "tensile")
 api.record(10, 10)
 api.optimise(10000, 200, 100, 0.65, 0.35)
