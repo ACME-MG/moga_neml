3d356f1f3fcd4c1ba745dd7c1b57eac5dd1249b7
janzen <janzenchoi@yahoo.com>
2023-06-28 04:06:05 -0500

    return evpcd record iters back

--- scripts/evpcd_main.py
+++ scripts/evpcd_main.py
@@ -15,5 +15,5 @@ api.add_error("y_end", "creep")
 api.read_file("tensile/AirBase_800_D7.csv", True)
 api.add_error("y_area", "tensile")
 
-api.record(1, 10)
-api.optimise(10000, 2, 1, 0.65, 0.35)
+api.record(10, 10)
+api.optimise(10000, 200, 100, 0.65, 0.35)
