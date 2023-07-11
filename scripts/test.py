8cf1a7de91ad39c9e54b3c3a9bfeb7dc838669bf
janzen <janzenchoi@yahoo.com>
2023-07-11 16:54:39 -0500

    test fix arg issue

--- scripts/test.py
+++ scripts/test.py
@@ -2,7 +2,7 @@ import sys; sys.path += [".."]
 from moga_neml.api import API
 
 api = API("", verbose=False, output_here=True)
-api.define_model("evp")
+api.define_model("evp", "helllooooo", 1233434)
 
 api.read_data("tensile/AirBase_900_D10.csv")
 api.remove_manual("strain", 0.4)
@@ -12,7 +12,7 @@ api.read_data("creep/inl_1/AirBase_900_28_G45.csv")
 api.read_data("creep/inl_1/AirBase_900_26_G59.csv")
 
 # params = (18.39, 30.036, 2.3999, 3.026, 2508.8, 0.80679, 5.4464, 3.8955)
-params = (4.871, 11.518, 7.0281, 4.2421, 1138.3)
-api.get_results(*params)
-api.plot_predicted(*params, type="tensile")
-api.plot_predicted(*params, type="creep")
+# params = (4.871, 11.518, 7.0281, 4.2421, 1138.3)
+# api.get_results(*params)
+# api.plot_predicted(*params, type="tensile")
+# api.plot_predicted(*params, type="creep")
