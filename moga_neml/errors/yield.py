585442bdcbf54f0cf465efaf8dabf8860351e5fd
janzen <janzenchoi@yahoo.com>
2023-07-26 11:06:41 -0500

    added some helper scripts, replaced tensile data, and added manual option for yield obj func

--- moga_neml/errors/yield.py
+++ moga_neml/errors/yield.py
@@ -11,21 +11,26 @@ import scipy.interpolate as inter
 import scipy.optimize as opt
 from moga_neml.errors.__error__ import __Error__
 from moga_neml._optimise.controller import BIG_VALUE
+from moga_neml._maths.interpolator import Interpolator
 
 # The Error class
 class Error(__Error__):
     
     # Runs at the start, once (optional)
-    def initialise(self, e_stress:float=1.0, offset:float=0.002):
+    def initialise(self, yield_stress:float=None, offset:float=0.002):
+
+        # Initialisation
         self.enforce_data_type("tensile")
         exp_data = self.get_exp_data()
-        
-        # Initialise input variables
-        self.e_stress = e_stress
         self.offset = offset
         
-        # Get yield point
-        self.exp_yield = self.get_yield(exp_data["strain"], exp_data["stress"])
+        # Get yield point manually or automatically, based on user input
+        if yield_stress != None:
+            interpolator = Interpolator(exp_data["stress"], exp_data["strain"])
+            yield_strain = interpolator.evaluate([yield_stress])[0]
+            self.exp_yield = (yield_strain, yield_stress)
+        else:
+            self.exp_yield = self.get_yield(exp_data["strain"], exp_data["stress"])
         self.mag_yield = math.sqrt(math.pow(self.exp_yield[0], 2) + math.pow(self.exp_yield[1], 2))
 
     # Computes the error value
@@ -37,19 +42,9 @@ class Error(__Error__):
         distance = math.sqrt(math.pow(self.exp_yield[0] - prd_yield[0], 2) + math.pow(self.exp_yield[1] - prd_yield[1], 2))
         return distance / self.mag_yield
 
-    # Gets the elastic modulus
-    def get_elastic_modulus(self, strain_list:list, stress_list:list) -> float:
-        new_strain_list, new_stress_list = [], []
-        for i in range(len(strain_list)):
-            if stress_list[i] < self.e_stress:
-                new_strain_list.append(strain_list[i])
-                new_stress_list.append(stress_list[i])
-        polynomial = np.polyfit(new_strain_list, new_stress_list, 1)
-        return polynomial[0]
-
     # Gets the yield point
     def get_yield(self, strain_list:list, stress_list:list) -> tuple:
-        youngs = self.get_elastic_modulus(strain_list, stress_list)
+        youngs = stress_list[1] / strain_list[1] # NEML produces noiseless curves
         sfn = inter.interp1d(strain_list, stress_list, bounds_error=False, fill_value=0)
         tfn = lambda e: youngs * (e - self.offset)
         yield_strain = opt.brentq(lambda e: sfn(e) - tfn(e), 0.0, np.max(strain_list))
--- scripts/evp_ansto.py
+++ scripts/evp_ansto.py
@@ -4,23 +4,20 @@ from moga_neml.api import API
 api = API("evp ansto")
 api.define_model("evp")
 
-api.read_data("tensile/ansto/AirBase_1e-2_A.csv")
+api.read_data("tensile/ansto/AirBase_1E-2_A.csv")
 api.remove_manual("strain", 0.4)
 api.add_error("y_area", "strain", "stress")
-api.add_error("yield", e_stress=400)
-api.add_error("hardening", strain_0=0.02)
+api.add_error("yield", yield_stress=560)
 
-api.read_data("tensile/ansto/AirBase_1e-3_C.csv")
+api.read_data("tensile/ansto/AirBase_1E-3_C.csv")
 api.remove_manual("strain", 0.4)
 api.add_error("y_area", "strain", "stress")
-api.add_error("yield", e_stress=300)
-api.add_error("hardening", strain_0=0.02)
+api.add_error("yield", yield_stress=440)
 
-api.read_data("tensile/ansto/AirBase_1e-4_C.csv") # B
+api.read_data("tensile/ansto/AirBase_1E-4_C.csv") # B
 api.remove_manual("strain", 0.4)
 api.add_error("y_area", "strain", "stress")
-api.add_error("yield", e_stress=200)
-api.add_error("hardening", strain_0=0.02)
+api.add_error("yield", yield_stress=380)
 
 api.reduce_errors("square_sum")
 api.reduce_objectives("square_sum")
