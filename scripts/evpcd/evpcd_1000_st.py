2f45c321d2b8410277d07d51443677e886036495
Janzen <janzenchoi@yahoo.com>
2023-09-15 10:48:18 +1000

    changed input files and rearranged some stuff

--- scripts/evpcd/evpcd_1000_st.py
+++ scripts/evpcd/evpcd_1000_st.py
@@ -33,5 +33,5 @@ api.reduce_errors("square_average")
 api.reduce_objectives("square_average")
 
 api.plot_experimental()
-api.set_recorder(50, 10, True)
+api.set_recorder(10, 10, True)
 api.optimise(10000, 100, 50, 0.65, 0.35)
