3bc37d38fb6eca9e82d781e4ff66f17de3efa2f6
Janzen <janzenchoi@yahoo.com>
2023-10-22 23:16:32 +1100

    Preparing for EVPWD testing' standby

--- moga_neml/models/evpwdb.py
+++ moga_neml/models/evpwdb.py
@@ -17,17 +17,23 @@ class Model(__Model__):
         """
         Runs at the start, once
         """
+
+        # Elastic parameters
         self.add_param("evp_s0",  0.0e0, 1.0e3) # 2
         self.add_param("evp_R",   0.0e0, 1.0e4) # 2
         self.add_param("evp_d",   0.0e1, 1.0e3) # 2
         self.add_param("evp_n",   1.0e0, 1.0e2) # 2
         self.add_param("evp_eta", 0.0e1, 1.0e6)
-        self.add_param("c_n",     1.0e0, 2.0e1)
-        self.add_param("c_0",     0.0e0, 1.0e0)
-        self.add_param("c_1",     0.0e0, 1.0e1)
-        self.add_param("t_n",     1.0e0, 2.0e1)
-        self.add_param("t_0",     0.0e0, 1.0e0)
-        self.add_param("t_1",     0.0e0, 1.0e1)
+        
+        # Creep damage parameters
+        self.add_param("c_n", 1.0e0, 2.0e1)
+        self.add_param("c_0", 0.0e0, 1.0e0)
+        self.add_param("c_1", 0.0e0, 1.0e1)
+
+        # Tensile damage parameters
+        self.add_param("t_n", 1.0e0, 2.0e1)
+        self.add_param("t_0", 0.0e0, 1.0e0)
+        self.add_param("t_1", 0.0e0, 1.0e1)
 
     def calibrate_model(self, evp_s0, evp_R, evp_d, evp_n, evp_eta, c_n, c_0, c_1, t_n, t_0, t_1):
         """
@@ -48,13 +54,20 @@ class Model(__Model__):
         integrator    = general_flow.TVPFlowRule(elastic_model, visco_model)
         evp_model     = models.GeneralIntegrator(elastic_model, integrator, verbose=False)
         
-        # Get work - work-rate interpolation
+        # Get work-work_rate interpolation
         try:
-            x_list, y_list = get_damage(c_0, c_1, t_0, t_1)
+            x_list, y_list = get_wc(c_0, c_1, t_0, t_1)
         except:
             return
         wd_wc = interpolate.PiecewiseLinearInterpolate(x_list, y_list)
         
+        # Get n-work_rate interpolation
+        try:
+            x_list, y_list = get_damage(c_0, c_1, t_0, t_1)
+        except:
+            return
+        
+
         # TODO temporary relation (should be interpolator too)
         wd_n = c_n if self.get_data("type") == "creep" else t_n # tensile
         
@@ -63,7 +76,7 @@ class Model(__Model__):
         evpwd_model = damage.NEMLScalarDamagedModel_sd(elastic_model, evp_model, wd_model, verbose=False)
         return evpwd_model
 
-def get_damage(a_0:float, a_1:float, b_0:float, b_1:float):
+def get_wc(a_0:float, a_1:float, b_0:float, b_1:float):
     """
     Gets the damage interpolation bilinear curve
     
