48df3c91704d42cfd997d3297ce1746ab81dcd7c
Janzen <janzenchoi@yahoo.com>
2023-11-18 16:30:41 +1100

    minor changes to scripts

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,32 +1,29 @@
 import sys; sys.path += [".."]
 from moga_neml.api import API
 
-api = API("plot")
+api = API("plot", output_here=True)
 api.define_model("evp")
 
-api.read_data("creep/inl_1/AirBase_900_36_G22.csv")
+api.read_data("creep/inl_1/AirBase_800_80_G25.csv")
+api.remove_damage()
+api.read_data("creep/inl_1/AirBase_800_70_G44.csv")
+api.remove_damage()
+api.read_data("creep/inl_1/AirBase_800_65_G33.csv")
 api.remove_damage(0.1, 0.7)
-api.add_error("area", "time", "strain")
-
-api.read_data("creep/inl_1/AirBase_900_31_G50.csv")
+api.read_data("creep/inl_1/AirBase_800_60_G32.csv")
 api.remove_damage(0.1, 0.7)
-api.add_error("area", "time", "strain")
+api.read_data("tensile/inl/AirBase_800_D7.csv")
+api.remove_manual("strain", 0.3)
 
-api.read_data("creep/inl_1/AirBase_900_28_G45.csv")
-api.remove_damage(0.1, 0.7)
-# api.add_error("area", "time", "strain")
+# api.read_data("creep/inl_1/AirBase_900_36_G22.csv")
+# api.remove_damage(0.1, 0.7)
+# api.read_data("creep/inl_1/AirBase_900_31_G50.csv")
+# api.remove_damage(0.1, 0.7)
+# api.read_data("creep/inl_1/AirBase_900_28_G45.csv")
+# api.remove_damage(0.1, 0.7)
+# api.read_data("creep/inl_1/AirBase_900_26_G59.csv")
+# api.remove_damage(0.1, 0.7)
+# api.read_data("tensile/inl/AirBase_900_D10.csv")
 
-api.read_data("creep/inl_1/AirBase_900_26_G59.csv")
-api.remove_damage(0.1, 0.7)
-# api.add_error("area", "time", "strain")
-
-api.read_data("tensile/inl/AirBase_900_D10.csv")
-api.add_error("area", "strain", "stress")
-
-api.reduce_errors("square_average")
-api.reduce_objectives("square_average")
-
-api.plot_experimental()
-api.get_results(4.871, 11.518, 7.0281, 4.2421, 1138.3)
-# api.plot_predicted(4.871, 11.518, 7.0281, 4.2421, 1138.3)
-# api.plot_predicted(3.4692, 11.414, 8.204, 4.304, 1127.5)
+api.plot_predicted(9.8172, 101.47, 1.4057, 4.6857, 1595.7)
+# api.plot_predicted(11.695, 101.28, 1.1411, 4.6827, 1595.5)
