a27707dec0ff214c48c33525515c6e9910049bb8
Janzen <janzenchoi@yahoo.com>
2023-11-21 16:21:42 +1100

    Minor changes in run scripts

--- scripts/evp/run_900.sh
+++ scripts/evp/run_900.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 nohup python3 evp_900_st.py &
-sleep 1 &
-nohup python3 evp_900_st.py &
-sleep 1 &
-nohup python3 evp_900_st.py &
-sleep 1 &
-nohup python3 evp_900_st.py &
-sleep 1 &
-
 nohup python3 evp_900_all.py &
-sleep 1 &
-nohup python3 evp_900_all.py &
-sleep 1 &
-nohup python3 evp_900_all.py &
-sleep 1 &
-nohup python3 evp_900_all.py &
-sleep 1 &
