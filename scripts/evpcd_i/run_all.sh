1c689e9ff6870ab5fd26d897657266964605893a
Janzen <janzenchoi@yahoo.com>
2023-11-29 18:42:45 +1100

    added strain obj for evpcd

--- scripts/evpcd_i/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nohup python3 evpcd_800_st.py &
-nohup python3 evpcd_800_all.py &
-nohup python3 evpcd_900_st.py &
-nohup python3 evpcd_900_all.py &
-# nohup python3 evpcd_1000_st.py &
-# nohup python3 evpcd_1000_all.py &
