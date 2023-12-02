37ce545640295898106b5c4035e086ef21cd9fe8
Janzen <janzenchoi@yahoo.com>
2023-12-03 10:50:17 +1100

    Changed parameters to evpcd model

--- scripts/evpcd_f/evpcd_800_all.py
+++ scripts/evpcd_f/evpcd_800_all.py
@@ -5,9 +5,9 @@ api = API("evpcd f 800 all", input_path="../data", output_path="../results")
 
 api.define_model("evpcd")
 
-fixed_params = "17.217	179.74	0.61754	4.4166	1783.5"
+# fixed_params = "17.217	179.74	0.61754	4.4166	1783.5"
 # fixed_params = "5.6908	66.627	1.9851	4.7723	1621.6"
-# fixed_params = "9.3076	32.596	5.8114	4.5263	1775.9"
+fixed_params = "9.3076	32.596	5.8114	4.5263	1775.9"
 # fixed_params = "5.8951	36.245	5.3757	4.7311	1598.4"
 # fixed_params = "4.1862	84.548	2.1123	4.7752	1574.3"
 # fixed_params = "25.038	90.693	0.61002	4.1982	1944.6"
