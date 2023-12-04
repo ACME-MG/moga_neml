1c562c835fde226ac6a4748412313e50c1f44fc6
Janzen <janzenchoi@yahoo.com>
2023-12-05 10:56:30 +1100

    New evpcd params

--- scripts/evpcd_f/evpcd_900_st.py
+++ scripts/evpcd_f/evpcd_900_st.py
@@ -8,8 +8,8 @@ api.define_model("evpcd")
 # fixed_params = "4.8238	378.86	0.1423	4.4021	1006.9"
 # fixed_params = "7.1037	48.235	4.2972	3.4084	1829.9"
 # fixed_params = "3.0461	19.608	32.421	3.2736	2022.3"
-fixed_params = "4.634	18.607	24.896	3.3436	1999.7"
-# fixed_params = "16.287	181.45	0.52517	3.0161	2606.2"
+# fixed_params = "4.634	18.607	24.896	3.3436	1999.7"
+fixed_params = "16.287	181.45	0.52517	3.0161	2606.2"
 # fixed_params = "9.5313	148.61	0.37484	3.9621	1253.9"
 # fixed_params = "6.3647	149.54	0.43913	4.1839	1127.6"
 # fixed_params = "12.622	15.441	4.5386	3.7305	1371.6"
@@ -39,7 +39,7 @@ api.remove_oxidation()
 api.read_data("tensile/inl/AirBase_900_D10.csv")
 api.add_error("area", "strain", "stress")
 api.add_error("end", "strain")
-api.add_error("arg_max", "strain", "stress")
+api.add_error("arg_max", "strain", "stress", weight=0.5)
 api.add_error("yield", yield_stress=164)
 # api.add_error("end", "stress")
 
--- scripts/evpwdb_f/evpwdb_900_all.py
+++ scripts/evpwdb_f/evpwdb_900_all.py
@@ -53,7 +53,7 @@ api.add_constraint("dec_end", "time")
 api.read_data("tensile/inl/AirBase_900_D10.csv")
 api.add_error("area", "strain", "stress")
 api.add_error("end", "strain")
-api.add_error("arg_max", "strain", "stress")
+api.add_error("arg_max", "strain", "stress", weight=0.5)
 api.add_error("yield", yield_stress=164)
 # api.add_error("end", "stress")
 
