fa09b3efcd9bc357d43d616dfe5cd00c6011da34
Janzen <janzenchoi@yahoo.com>
2023-12-14 00:10:44 +1100

    added set_custom_driver api function and removed set_driver api function

--- scripts/ondrej/run_evpy.py
+++ scripts/ondrej/run_evpy.py
@@ -16,6 +16,5 @@ api.remove_manual("strain", 0.014)
 api.add_error("area", "strain", "stress")
 api.add_error("end", "strain")
 
-api.set_driver(max_strain=0.014)
 api.set_recorder(1, True, True, True)
 api.optimise(50, 50, 25, 0.8, 0.01)
