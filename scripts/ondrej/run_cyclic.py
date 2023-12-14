e2f440d2b772aa3dde6dca5a49abddac4a8a2b8e
Janzen <janzenchoi@yahoo.com>
2023-12-15 00:01:34 +1100

    Remove unnecessary scaling; everything should be in SI now

--- scripts/ondrej/run_cyclic.py
+++ scripts/ondrej/run_cyclic.py
@@ -1,28 +1,37 @@
 import sys; sys.path += ["../.."]
 from moga_neml.api import API
 
-model_name = "riclih"
+# model_name = "cvih"
+# model_name = "riclih"
+# model_name = "rilih"
+# model_name = "rilikh"
+# model_name = "riplih"
+# model_name = "rivih"
+# model_name = "vpclih"
+# model_name = "vpcvih"
+# model_name = "vpplih"
+model_name = "vppvih"
 api = API(model_name, input_path="../data", output_path="../results")
 api.define_model(model_name)
 
 api.read_data("cyclic/Airbase316.csv", num_points=5000)
-api.change_data("num_cycles", 2)
-api.remove_manual("time", 256)
-api.add_error("area", "time", "strain", num_points=200)
-api.add_error("area", "time", "stress", num_points=200)
-api.add_error("max", "strain", weight=10)
-api.add_error("max", "stress", weight=10)
-api.add_error("end", "time", weight=10)
+# api.change_data("num_cycles", 2)
+api.remove_manual("time", 252)
+api.add_error("area", "time", "strain", num_points=1500)
+api.add_error("area", "time", "stress", num_points=1500)
+api.add_error("max", "strain")
+api.add_error("max", "stress")
 api.add_error("num_peaks", "time", "strain")
+api.add_error("peak_dist", "time", "strain")
 
-params_str = """
-180.0	165.0	80.0e3	14.02e3	0.9e3	1.5e3
-"""
-params_list = [list(map(float, line.split())) for line in params_str.strip().split("\n")]
-for params in params_list:
-    api.plot_prediction(*params)
-    api.get_results(*params)
-exit()
+# params_str = """
+# 180.0	165.0	80.0e3	14.02e3	0.9e3	1.5e3
+# """
+# params_list = [list(map(float, line.split())) for line in params_str.strip().split("\n")]
+# for params in params_list:
+#     api.plot_prediction(*params)
+#     api.get_results(*params)
+# exit()
 
 api.plot_experimental()
 api.set_recorder(1, plot_opt=True)
