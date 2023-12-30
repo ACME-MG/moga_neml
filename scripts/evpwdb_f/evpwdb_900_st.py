6e0daa3e4aaaac0e734dcfd015709353d20f695f
Janzen <janzenchoi@yahoo.com>
2023-12-30 12:58:07 +1100

    New evpwd params

--- scripts/evpwdb_f/evpwdb_900_st.py
+++ scripts/evpwdb_f/evpwdb_900_st.py
@@ -18,7 +18,7 @@ params_str = """
 7.044	16.175	6.7949	4.1207	1090
 """
 params_list = [list(map(float, line.split())) for line in params_str.strip().split("\n")]
-api.fix_params(params_list[6])
+api.fix_params(params_list[7])
 
 api.read_data("creep/inl_1/AirBase_900_36_G22.csv")
 api.add_error("area", "time", "strain")
