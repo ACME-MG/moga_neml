df31d8b2bbc89b9e98e3981ac5bc01b1b4dee709
janzen <janzenchoi@yahoo.com>
2024-01-18 15:47:38 +1100

    Increase driver iter to 2000; updated SEM results

--- scripts/plot_sem/plot_evpwd_900.py
+++ scripts/plot_sem/plot_evpwd_900.py
@@ -9,17 +9,19 @@ itf.add_error("dummy")
 itf.read_data("creep/inl_1/AirBase_900_31_G50.csv")
 itf.add_error("dummy")
 itf.read_data("creep/inl_1/AirBase_900_28_G45.csv")
-# itf.add_error("dummy")
+itf.add_error("dummy")
 itf.read_data("creep/inl_1/AirBase_900_26_G59.csv")
 itf.remove_oxidation()
-# itf.add_error("dummy")
+itf.add_error("dummy")
 itf.read_data("tensile/inl/AirBase_900_D10.csv")
 itf.add_error("dummy")
 
 params_str = """
-16.287	181.45	0.52517	3.0161	2606.2	1.7053	0.25847	2.6939	15.437	0.92752	2.6376
-16.287	181.45	0.52517	3.0161	2606.2	2.0023	0.3443	3.0473	17.21	0.80515	2.7325
-16.287	181.45	0.52517	3.0161	2606.2	1.9568	0.29809	2.809	11.219	0.99315	1.7022
+4.871	11.518	7.0281	4.2421	1138.3	2.3686	44.152	268.35	6.2109	792.75	736.88
+4.871	11.518	7.0281	4.2421	1138.3	6.9671	39.733	230.37	11.257	370.51	701.98
+4.871	11.518	7.0281	4.2421	1138.3	1.6383	97.283	551.73	5.8218	205.44	960.96
+4.871	11.518	7.0281	4.2421	1138.3	6.7179	198.85	597.73	7.206	417.21	692.63
+4.871	11.518	7.0281	4.2421	1138.3	1.8284	56.416	340.6	12.401	644.04	765.58
 
 """
 params_list = [list(map(float, line.split())) for line in params_str.strip().split("\n")]
