af2a4cf0ae1f6e61535ca29620495f54c2475674
Janzen <janzenchoi@yahoo.com>
2024-02-05 17:25:22 +1100

    slightly altered scripts; minor changes throughout

--- scripts/evpwdb_f/run_all.sh
+++ scripts/evpwdb_f/run_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=1
-counter=5
+counter=10
 while [ $counter -gt 0 ]; do
     echo "run $counter"
-    # nohup python3 evpcd_800_st.py &
-    # nohup python3 evpcd_900_st.py &
-    nohup python3 evpcd_1000_st.py &
+    # nohup python3 evpwdb_800_st.py &
+    # nohup python3 evpwdb_900_st.py &
+    nohup python3 evpwdb_1000_st.py &
     sleep 1
     ((counter--))
 done
