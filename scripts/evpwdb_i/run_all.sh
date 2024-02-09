577e89cc7947088bea277c117f3c3875ccce63e3
Janzen <janzenchoi@yahoo.com>
2024-02-09 11:54:14 +1100

    removed work damage results

--- scripts/evpwdb_i/run_all.sh
+++ scripts/evpwdb_i/run_all.sh
@@ -4,8 +4,8 @@ export OMP_NUM_THREADS=1
 counter=10
 while [ $counter -gt 0 ]; do
     echo "run $counter"
-    # nohup python3 evpwdb_800_st.py &
-    # nohup python3 evpwdb_900_st.py &
+    nohup python3 evpwdb_800_st.py &
+    nohup python3 evpwdb_900_st.py &
     nohup python3 evpwdb_1000_st.py &
     sleep 1
     ((counter--))
